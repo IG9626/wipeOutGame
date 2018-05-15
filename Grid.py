3afc42d2b408aaf0e05f3e566e6abdd49939650e
IG9626 <31745226+IG9626@users.noreply.github.com>
2018-05-15 14:13:45 -0600

    Add files via upload

---
+++ Grid.py
@@ -0,0 +1,4 @@
+from Tile import Tile
+defaultTile = Tile(0,0)
+grid = [[defaultTile for xSize in range(8)] for ySize in range(8)]
+return grid
---
+++ wipeOutGame.pyde
@@ -0,0 +1,5 @@
+#5-15-18 started at 1:38PM stoped at 1:50
+def setup():
+    size(500,500)
+    background(150)
+    
